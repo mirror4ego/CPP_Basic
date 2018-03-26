c622cb89458ad7bfa8633ed533b9ff9fa8d47ccc
Young-Desktop <mirror4ego@naver.com>
2018-03-27 08:37:21 +0900

    기본 소스 완성

---
+++ chap13/Exam13_07/MyStack.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "MyStack.h"
+
+void MyStack::push(int n) {
+	if (tos == 99)
+		throw "Stack Full";
+	tos++;
+	data[tos] = n;
+}
+
+int MyStack::pop() {
+	if (tos == -1)
+		throw "Stack Empty";
+	int rData = data[tos--];
+	return rData;
+}
